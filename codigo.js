88837ab09815dcd09406bd2612ebde4f499a3925
Lucas Rodi <lmrodi003@gmail.com>
2022-10-10 15:05:36 -0300

    holabro

--- codigo.js
+++ codigo.js
@@ -17,20 +17,23 @@ if(operacion==1){
     let num1 = prompt("¿primer numero que deseas sumar?");
     let num2 = prompt("¿segundo numero que deseas sumar?");
     let resultado=sumar(num1,num2);
-    alert(resultado)
+    alert(`el resultado es ${resultado}`)
 } else if(operacion==2){
     let num1 = prompt("¿primer numero que deseas restar?");
     let num2 = prompt("¿segundo numero que deseas restar?");
     let resultado=restar(num1,num2);
-    alert(resultado)
+    alert(`el resultado es ${resultado}`)
 } else if(operacion==3){
     let num1 = prompt("¿primer numero que deseas dividir?");
     let num2 = prompt("¿segundo numero que deseas dividir?");
     let resultado=dividir(num1,num2);
-    alert(resultado)
+    alert(`el resultado es ${resultado}`)
+
 }else if(operacion==4){
     let num1 = prompt("¿primer numero que deseas multiplicar?");
     let num2 = prompt("¿segundo numero que deseas multiplicar?");
     let resultado=multiplicar(num1,num2);
-    alert(resultado)
+    alert(`el resultado es ${resultado}`)
+}else{
+    alert("no se selecciono ninguna operacion")
 }
