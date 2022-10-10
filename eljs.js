6f9d36178eb1ae9a20228617a59e1edd31a01f79
Lucas Rodi <lmrodi003@gmail.com>
2022-10-10 00:54:50 -0300

    agregando calculador basica

--- eljs.js
+++ eljs.js
@@ -1,1 +1,36 @@
-alert("hola")
+const sumar = (num1,num2)=>{
+    return parseInt(num1) + parseInt(num2);
+}
+const restar = (num1,num2)=>{
+    return parseInt(num1) - parseInt(num2);
+}
+const dividir = (num1,num2)=>{
+    return parseInt(num1) / parseInt(num2);
+}
+const multiplicar = (num1,num2)=>{
+    return parseInt(num1) * parseInt(num2);
+}
+
+alert("¿Qué operación deseas realizar?")
+let operacion = prompt("1= Sumar, 2 =Restar, 3 =Dividir, 4 =Multiplicar");
+if(operacion==1){
+    let num1 = prompt("¿primer numero que deseas sumar?");
+    let num2 = prompt("¿segundo numero que deseas sumar?");
+    let resultado=sumar(num1,num2);
+    alert(resultado)
+} else if(operacion==2){
+    let num1 = prompt("¿primer numero que deseas restar?");
+    let num2 = prompt("¿segundo numero que deseas restar?");
+    let resultado=restar(num1,num2);
+    alert(resultado)
+} else if(operacion==3){
+    let num1 = prompt("¿primer numero que deseas dividir?");
+    let num2 = prompt("¿segundo numero que deseas dividir?");
+    let resultado=dividir(num1,num2);
+    alert(resultado)
+}else if(operacion==4){
+    let num1 = prompt("¿primer numero que deseas multiplicar?");
+    let num2 = prompt("¿segundo numero que deseas multiplicar?");
+    let resultado=multiplicar(num1,num2);
+    alert(resultado)
+}
